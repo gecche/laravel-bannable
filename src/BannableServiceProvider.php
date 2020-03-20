99430ee9c936fe5e57d24bdea2c040d6598e1c1a
Giacomo Terreni <giacomoterreni@gmail.com>
2020-03-20 10:40:42 +0100

    migrations in publish

--- src/BannableServiceProvider.php
+++ src/BannableServiceProvider.php
@@ -27,7 +27,11 @@ class BannableServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+//        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         Auth::provider('eloquent-bannable', function ($app, array $config) {
             // Return an instance of Illuminate\Contracts\Auth\UserProvider...
